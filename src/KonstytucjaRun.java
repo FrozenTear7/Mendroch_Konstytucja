2cc143ed22cea7c512b3ddb8052b24770899c0ed
FrozenTear7 <pawelmendroch7@gmail.com>
2017-12-02 21:32:45 +0100

    Add file reader with custom user input

--- src/KonstytucjaRun.java
+++ src/KonstytucjaRun.java
@@ -1,5 +1,31 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class KonstytucjaRun {
     public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj ścieżkę do pliku: ");
+        //String filePath = input.next();
+        String filePath = "./konstytucja.txt";
+        String finalInput;
 
+        StringBuilder contentBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(
+                new FileInputStream(filePath), "Cp1250")))
+        {
+
+            String sCurrentLine;
+            while ((sCurrentLine = br.readLine()) != null)
+            {
+                contentBuilder.append(sCurrentLine).append("\n");
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        finalInput = contentBuilder.toString();
+        System.out.println(finalInput);
     }
 }
