9e1584c3362940d9e1e1e6798b1a94b58d0a1257
FrozenTear7 <pawelmendroch7@gmail.com>
2017-12-02 22:06:06 +0100

    Erase dates and "Kancelaria sejmu" from the input

--- src/KonstytucjaRun.java
+++ src/KonstytucjaRun.java
@@ -4,24 +4,26 @@ import java.util.Scanner;
 public class KonstytucjaRun {
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Podaj ścieżkę do pliku: ");
+        //System.out.println("Podaj ścieżkę do pliku: ");
         //String filePath = input.next();
         String filePath = "./konstytucja.txt";
         String finalInput;
 
         StringBuilder contentBuilder = new StringBuilder();
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(
-                new FileInputStream(filePath), "Cp1250")))
-        {
-
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), "Cp1250"))) {
             String sCurrentLine;
-            while ((sCurrentLine = br.readLine()) != null)
-            {
-                contentBuilder.append(sCurrentLine).append("\n");
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                if (sCurrentLine.matches(".*\\d{4}-\\d{2}-\\d{2}.*") || sCurrentLine.matches(".*Kancelaria Sejmu.*"))
+                    contentBuilder.append("");
+                else if(sCurrentLine.endsWith("-"))
+                    contentBuilder.append(sCurrentLine.substring(0, sCurrentLine.length()-1));
+                else if (!sCurrentLine.endsWith(",") && !sCurrentLine.endsWith("."))
+                    contentBuilder.append(sCurrentLine).append(" ");
+                else
+                    contentBuilder.append(sCurrentLine).append("\n");
             }
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
