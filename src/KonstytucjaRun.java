dea284f6d62ad1a24bf544a6de8dc7aa01780ce9
FrozenTear7 <RainbowPablo7@gmail.com>
2017-12-07 18:23:43 +0100

    Add working input section in main

--- src/KonstytucjaRun.java
+++ src/KonstytucjaRun.java
@@ -3,26 +3,34 @@ import java.util.Scanner;
 public class KonstytucjaRun {
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
+        int range1 = 0, range2 = 0;
+        String filePath, mode, element, range;
+
+        //String filePath = "./konstytucja.txt", mode = "T", element = "Rozdział", range = "Z";
+        //int range1 = 1, range2 = 3;
+
         System.out.println("Podaj ścieżkę do pliku: ");
-        //String filePath = input.next();
-        String filePath = "./konstytucja.txt", mode = "T", element = "Rozdział", range = "Z";
-        int range1 = 1, range2 = 3;
+        filePath = input.next();
+
         System.out.println("Podaj tryb działania programu: S - Spis treści, T - Treść podanego pliku");
-        //String mode = input.next();
+        mode = input.next();
+
         System.out.println("Podaj element, który chcesz wyświetlić:\nLEGENDA:\n" +
                 "Rozdział, Sekcja, Artykuł, Ustęp, Pkt\n");
-        //String element = input.next();
+        element = input.next();
+
         System.out.println("Podaj tryb wyświetlenia elementu: P - Pojedynczy element, Z - Zakres elementów");
-        //String range = input.next();
+        range = input.next();
+
         if(range.equals("P")) {
             System.out.println("Podaj numer elementu do wyświetlenia");
-            //String range1 = input.next();
+            range1 = Integer.parseInt(input.next());
         }
         if(range.equals("Z")) {
             System.out.println("Podaj początek zakresu elementów do wyświetlenia");
-            //String range1 = input.next();
+            range1 = Integer.parseInt(input.next());
             System.out.println("Podaj koniec zakresu elementów do wyświetlenia");
-            //String range2 = input.next();
+            range2 = Integer.parseInt(input.next());
         }
 
         InputParser inputParser = new InputParser(filePath, mode, element, range1, range2);
